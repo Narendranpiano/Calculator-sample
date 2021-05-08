0d385cd7b361e4df67bdba987f3b180b09f246bb
Narendran Bhaskaran <narendranpiano@gmail.com>
2021-05-08 15:15:49 +0200

    Frame is made

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,18 @@
+import javax.swing.JFrame;
 
 public class Calculator {
 
 	public Calculator() {
 		
+		JFrame jf=new JFrame("Calculator");
+		jf.setLayout(null);
+		jf.setSize(600, 600);
+		
+		jf.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		Calculator c=new Calculator();
+		new Calculator();
 	}
 }
